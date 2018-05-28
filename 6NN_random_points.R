99be75e0d6c43b70549da77ec7797dade69fd06f
edem995 <33476323+edem995@users.noreply.github.com>
2018-05-28 18:17:12 +0300

    Update 6NN_random_points.R

--- 6NN_random_points.R
+++ 6NN_random_points.R
@@ -1,48 +1,41 @@
 colors <- c("setosa" = "red", "versicolor" = "green3", "virginica" = "blue")
 plot(iris[, 3:4], pch = 21, bg = colors[iris$Species], col = colors[iris$Species], asp = 1)
-title("6NN random points")
+title("1NN")      ## Отобразим выборку, используя два последних признака таблицы:
 
 euclideanDistance <- function(u, v){
   sqrt(sum((u - v)^2))
-}
+}                             ## формула обычное евклидово расстояние
 
 sortObjectsByDist <- function(xl, z, metricFunction = euclideanDistance){
   l <- dim(xl)[1]
-  n <- dim(xl)[2] - 1
+  n <- dim(xl)[2] - 1                        ## Сортируем объекты согласно расстояния до объекта z
   
   distances <- matrix(NA, l, 2)
   
   for (i in 1:l){
     distances[i, ] <- c(i, metricFunction(xl[i, 1:n], z))
-  }
+  }                                           ## Создаём матрицу расстояний
   
   orderedXl <- xl[order(distances[, 2]), ]
   
   return (orderedXl);
-}
+} ## Сортируем
 
-kNN <- function(xl, z, k){
+kNN <- function(xl, z, k){                     ## Применяем метод kNN
   orderedXl <- sortObjectsByDist(xl, z)
-  n <- dim(orderedXl)[2] - 1
-  classes <- orderedXl[1:k, n + 1]
-  counts <- table(classes)
+  n <- dim(orderedXl)[2] - 1                 ## Сортируем выборку согласно классифицируемого объекта       
+  classes <- orderedXl[1:k, n + 1]          ## Получаем классы первых k соседей
+  counts <- table(classes)                    ## Составляем таблицу встречаемости каждого класса
   class <- names(which.max(counts))
   
   return (class)
-}
+}                                       ## Находим класс, который доминирует среди первых k соседей
 
-painting <- function(x, y) {
-  z <- c(x, y)
-  xl <- iris[, 3:5]
-  class <- kNN(xl, z, k=6)
-  points(z[1], z[2], pch = 22, bg = colors[class], asp = 1)
-}
+colors <- c("setosa" = "red", "versicolor" = "green3", "virginica" = "blue")
+plot(iris[, 3:4], pch = 21, bg = colors[iris$Species], col
+= colors[iris$Species], asp = 1)            ## Рисуем выборку
 
-testX <- runif(50, 1.0, 7.0)
-testY <- runif(50, 0, 2.5)
-
-for (i in 1:50){
-  painting(testX[i], testY[i])
-}
-
-legend("bottomright", c("setosa", "versicolor", "virginica"), pch = c(15,15,15), col = c("red", "green3", "blue"))
+z <- c(2.7, 1)
+xl <- iris[, 3:5]
+class <- kNN(xl, z, k=6)
+points(z[1], z[2], pch = 22, bg = colors[class], asp = 1)   ## Классификация одного заданного объекта
